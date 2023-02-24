99f26cb38b845347842a6fbaed65a612693ba28b
Szymon Zyguła <szymon@zygula.eu>
2023-02-24 23:48:51 +0100

    Fix and extend matrices

--- src/math/matrix.rs
+++ src/math/matrix.rs
@@ -7,6 +7,16 @@ pub struct Matrix<T: Float, const M: usize, const N: usize> {
 }
 
 impl<T: Float, const M: usize, const N: usize> Matrix<T, M, N> {
+    pub fn zero() -> Matrix<T, M, N> {
+        let data: [[T; M]; N] = [[T::from(0.0).unwrap(); M]; N];
+        Matrix::<T, M, N> { data }
+    }
+
+    pub unsafe fn uninit() -> Matrix<T, M, N> {
+        let data: [[T; M]; N] = std::mem::MaybeUninit::uninit().assume_init();
+        Matrix::<T, M, N> { data }
+    }
+
     pub fn at(&self, row: usize, col: usize) -> T {
         self.data[col][row]
     }
@@ -15,16 +25,28 @@ impl<T: Float, const M: usize, const N: usize> Matrix<T, M, N> {
         &mut self.data[col][row]
     }
 
-    pub fn transpose(&self) -> Matrix<T, M, N> {
-        let mut data: [[T; M]; N] = unsafe { std::mem::MaybeUninit::uninit().assume_init() };
+    pub fn transpose(&self) -> Matrix<T, N, M> {
+        let mut result = unsafe { Matrix::<T, N, M>::uninit() };
 
         for i in 0..M {
             for j in 0..N {
-                data[i][j] = self.data[j][i];
+                result.data[i][j] = self.data[j][i];
             }
         }
 
-        Matrix::<T, M, N> { data }
+        result
+    }
+}
+
+impl<T: Float, const M: usize> Matrix<T, M, M> {
+    pub fn diagonal(diagonal_values: &[T; M]) -> Matrix<T, M, M> {
+        let mut result = Matrix::<T, M, M>::zero();
+
+        for i in 0..M {
+            result.data[i][i] = diagonal_values[i];
+        }
+
+        result
     }
 }
 
@@ -34,40 +56,38 @@ impl<T: Float> Matrix<T, 1, 1> {
     }
 }
 
-impl<T: Float, const M: usize, const N: usize> std::ops::Add<&Matrix<T, M, N>>
-    for &Matrix<T, M, N>
-{
+impl<T: Float, const M: usize, const N: usize> std::ops::Add<Matrix<T, M, N>> for Matrix<T, M, N> {
     type Output = Matrix<T, M, N>;
 
-    fn add(self, rhs: &Matrix<T, M, N>) -> Self::Output {
-        let mut data: [[T; M]; N] = unsafe { std::mem::MaybeUninit::uninit().assume_init() };
+    fn add(self, rhs: Matrix<T, M, N>) -> Self::Output {
+        let mut result = unsafe { Self::Output::uninit() };
 
         for i in 0..M {
             for j in 0..N {
-                data[i][j] = self.data[i][j] + rhs.data[i][j];
+                result.data[i][j] = self.data[i][j] + rhs.data[i][j];
             }
         }
 
-        Self::Output { data }
+        result
     }
 }
 
 impl<T: Float + std::ops::AddAssign<T>, const M: usize, const N: usize, const L: usize>
-    std::ops::Mul<&Matrix<T, N, L>> for &Matrix<T, M, N>
+    std::ops::Mul<Matrix<T, N, L>> for Matrix<T, M, N>
 {
     type Output = Matrix<T, M, L>;
 
-    fn mul(self, rhs: &Matrix<T, N, L>) -> Matrix<T, M, L> {
-        let mut data: [[T; M]; L] = [[T::from(0.0).unwrap(); M]; L];
+    fn mul(self, rhs: Matrix<T, N, L>) -> Self::Output {
+        let mut result = Self::Output::zero();
 
         for i in 0..M {
             for j in 0..N {
                 for k in 0..L {
-                    data[i][j] += self.data[k][j] * rhs.data[i][k];
+                    result.data[i][j] += self.data[k][j] * rhs.data[i][k];
                 }
             }
         }
 
-        Matrix { data }
+        result
     }
 }
