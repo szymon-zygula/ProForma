2b8df88e44a86648f7af47194500b3de77383c33
Szymon Zyguła <szymon@zygula.eu>
2023-02-27 22:29:42 +0100

    Add Gaussian Elimination with Partial Pivoting, linear equation system solving and matrix inverse

--- src/math/matrix.rs
+++ src/math/matrix.rs
@@ -6,7 +6,7 @@ pub struct Matrix<T: Float, const M: usize, const N: usize> {
     data: [[T; N]; M],
 }
 
-impl<T: Float, const M: usize, const N: usize> Matrix<T, M, N> {
+impl<T: Float + Copy, const M: usize, const N: usize> Matrix<T, M, N> {
     pub fn zero() -> Matrix<T, M, N> {
         let data = [[T::from(0.0).unwrap(); N]; M];
         Matrix::<T, M, N> { data }
@@ -57,6 +57,70 @@ impl<T: Float, const M: usize, const N: usize> Matrix<T, M, N> {
 
         result
     }
+
+    pub fn swap_rows(&mut self, row1: usize, row2: usize) {
+        let copy = self.data[row1];
+        self.data[row1] = self.data[row2];
+        self.data[row2] = copy;
+    }
+
+    pub fn add_row1_to_row2(&mut self, row1: usize, row2: usize, multiplier: T) {
+        for col in 0..N {
+            self.data[row2][col] = self.data[row2][col] + multiplier * self.data[row1][col];
+        }
+    }
+
+    pub fn mutiply_row(&mut self, row: usize, multiplier: T) {
+        for col in 0..N {
+            self.data[row][col] = multiplier * self.data[row][col];
+        }
+    }
+
+    pub fn apply_row_ops(&mut self, row_ops: &[RowOp<T>]) {
+        for op in row_ops {
+            match op {
+                RowOp::Swap(row1, row2) => self.swap_rows(*row1, *row2),
+                RowOp::Add(row1, row2, mul) => self.add_row1_to_row2(*row1, *row2, *mul),
+                RowOp::Mul(row, mul) => self.mutiply_row(*row, *mul),
+            }
+        }
+    }
+
+    pub fn gaussian_elimination_with_partial_pivoting(
+        &self,
+    ) -> Option<(Matrix<T, M, N>, Vec<RowOp<T>>)> {
+        let mut gepp_matrix = *self;
+        let mut operations = Vec::with_capacity(M * N);
+
+        for i in 0..std::cmp::min(M, N) {
+            let mut pivot = i;
+
+            while gepp_matrix.data[i][pivot] == T::from(0.0).unwrap() {
+                pivot += 1;
+            }
+
+            if pivot == N {
+                return None;
+            }
+
+            if pivot != i {
+                operations.push(RowOp::Swap(i, pivot));
+                gepp_matrix.swap_rows(i, pivot);
+            }
+
+            let multiplier = T::from(1.0).unwrap() / gepp_matrix.data[i][i];
+            operations.push(RowOp::Mul(i, multiplier));
+            gepp_matrix.mutiply_row(i, multiplier);
+
+            for row in (i + 1)..M {
+                let multiplier = -gepp_matrix.data[row][i];
+                operations.push(RowOp::Add(i, row, multiplier));
+                gepp_matrix.add_row1_to_row2(i, row, multiplier);
+            }
+        }
+
+        Some((gepp_matrix, operations))
+    }
 }
 
 impl<T: Float, const M: usize> Matrix<T, M, M> {
@@ -72,6 +136,37 @@ impl<T: Float, const M: usize> Matrix<T, M, M> {
         }
 
         result
+    }
+
+    pub fn diagonalization_of_gaussed(&self) -> Vec<RowOp<T>> {
+        let mut diagonal = *self;
+        let mut row_ops = Vec::new();
+
+        for i in (0..M).rev() {
+            for j in 0..i {
+                let multiplier = -diagonal.data[j][i];
+                row_ops.push(RowOp::Add(i, j, multiplier));
+                diagonal.add_row1_to_row2(i, j, multiplier);
+            }
+        }
+
+        row_ops
+    }
+
+    pub fn solve_linear_system<const L: usize>(
+        &self,
+        mut constant_terms: Matrix<T, M, L>,
+    ) -> Option<Matrix<T, M, L>> {
+        let (upper_triangular, gauss_ops) = self.gaussian_elimination_with_partial_pivoting()?;
+        let solution_ops = upper_triangular.diagonalization_of_gaussed();
+
+        constant_terms.apply_row_ops(&gauss_ops);
+        constant_terms.apply_row_ops(&solution_ops);
+        Some(constant_terms)
+    }
+
+    pub fn inverse(&self) -> Option<Matrix<T, M, M>> {
+        self.solve_linear_system(Matrix::<T, M, M>::identity())
     }
 }
 
@@ -144,4 +239,11 @@ where
 
         Ok(())
     }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum RowOp<T: Float> {
+    Swap(usize, usize),
+    Add(usize, usize, T),
+    Mul(usize, T),
 }
